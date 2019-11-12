35f0d70e111436ab871bd24d025023a114a2b9e5
kendrang <kendrang@users.noreply.github.com>
2019-11-12 17:47:45 -0500

    done people methods finally

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,4 +1,53 @@
 package io.zipcoder.interfaces;
 
-public class People {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class People implements Iterable <> {
+  List<Person> personList;
+
+  public void add (Person person){
+    personList.add(person);
+  }
+  public Person findById(long id){
+    for (Person p : personList){
+      if(p.getId() ==id){
+        return p;
+      }
+    }return null;
+  }
+
+  public boolean contains (Person person){
+    if (personList.contains(person)){
+      return true;
+    }return false;
+  }
+
+  public void remove (Person person){
+    personList.remove(person);
+
+  }
+
+  public void remove(long id){
+   personList.remove(findById(id));
+  }
+
+  public void removeAll (){
+    personList.clear();
+  }
+
+  public Integer count(){
+   return personList.size();
+  }
+
+  public Person[] toArray (){
+   return (Person[]) personList.toArray();
+  }
+
+  @Override
+  public Iterator iterator() {
+    return personList.iterator();
+  }
 }
