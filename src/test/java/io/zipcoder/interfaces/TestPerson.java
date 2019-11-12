259e9be99c29ca09cfb85d7d57464ad8b29b3312
kendrang <kendrang@users.noreply.github.com>
2019-11-12 16:03:20 -0500

    one test

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,21 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor() {
+        long expectedID = 1L;
+        String expectedName = "Jimmyjohn";
+        Person testedPerson = new Person(expectedID, expectedName);
+        long actualID = testedPerson.getId();
+        String actualName = testedPerson.getName();
+        Assert.assertEquals(testedPerson.getId(), actualID);
+        Assert.assertEquals(testedPerson.getName(),actualName);
+
+
+    }
 
 }
