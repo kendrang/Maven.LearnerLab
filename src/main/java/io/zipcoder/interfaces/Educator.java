2054214de5743f9d75603f36bc3bdf9afa53e87d
kendrang <kendrang@users.noreply.github.com>
2019-11-14 17:05:22 -0500

    almost done

---
+++ src/main/java/io/zipcoder/interfaces/Educator.java
@@ -0,0 +1,29 @@
+package io.zipcoder.interfaces;
+
+public enum Educator implements Teacher{
+
+   DOLIO(1, "Dolio"), FROILAN(2,"Froilan"), ROBERTO(3, "Roberto");
+
+    final Instructor instructor;
+    double timeWorked= 0;
+
+    Educator(int i, String name) {
+        instructor = new Instructor(i, "");
+    };
+
+
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+        instructor.teach(learner,numberOfHours);
+        timeWorked += numberOfHours;
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+        instructor.lecture(learners,numberOfHours);
+        timeWorked += numberOfHours;
+    }
+
+
+}
